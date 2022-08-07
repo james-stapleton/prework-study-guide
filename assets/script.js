186e7c9b451b45cde52f2f9f680c9f964542a606
james-stapleton <jstapleton536@gmail.com>
2022-08-07 16:18:52 -0400

    Added comment to JS code

--- assets/script.js
+++ assets/script.js
@@ -22,6 +22,7 @@ function selectTopic() {
 }
 
 console.log('Here are the topics we learned through Prework:');
+// remember to call functions
 listTopics();
 console.log('Which topic should we study first?');
 selectTopic();
